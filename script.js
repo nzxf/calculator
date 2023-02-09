fd3c82644d73204071eda8fd1e806c2449ab3014
nzxf <122195155+nzxf@users.noreply.github.com>
2023-02-09 18:40:41 +0700

    Make add; subtract; multiply; divide function

---
+++ script.js
@@ -0,0 +1,17 @@
+
+//add
+const add = function (x, y) {
+    return x + y;
+};
+//substract
+const subtract = function (x, y) {
+    return x - y;
+};
+//multiply
+const multiply = function (x, y) {
+    return x * y;
+};
+//divide
+const divide = function (x, y) {
+    return x / y;
+};
