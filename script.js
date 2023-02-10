2341d76e1001cd20072d7cbfdd29ccfbe602b87b
nzxf <122195155+nzxf@users.noreply.github.com>
2023-02-10 20:45:29 +0700

    Fix operate function and equal event listener

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 //add
 const add = function (x, y) {
     return x + y;
 };
-//substract
+//subtract
 const subtract = function (x, y) {
     return x - y;
 };
@@ -15,67 +14,81 @@ const multiply = function (x, y) {
 const divide = function (x, y) {
     return x / y;
 };
-
 //operate
-const operate = function (operator, x, y) {
-    return operator(x, y);
-};
+const operate = function (x, oprtr, y) {
+    switch (oprtr) {
+        case "+":
+            return add(x, y)
+        case "-":
+            return subtract(x, y)
+        case "x":
+            return multiply(x, y)
+        case ":":
+            return divide(x, y)
+        default:
+            display.innerText = 'help'
+    };
+}
 
 //display
-const display = document.querySelector('#display');
-
-//input digit
-let inputDigit = []
-const digits = document.querySelectorAll('.digit');
-digits.forEach(digit => digit.addEventListener('click', function () {
-    inputDigit.push(digit.value)
-    // console.log(inputDigit)
-    display.innerText = inputDigit.join('')
-}
-))
-
-//input operator
-let inputOperator = ''
-let board = []
-const operators = document.querySelectorAll('#operator-container button')
-operators.forEach(operator => operator.addEventListener('click', function () {
-    board.push(inputDigit.join(''))
-    inputDigit = []
-    inputOperator = operator.value
-    // console.log(inputOperator)
-}))
+const display = document.querySelector('#display')
+display.innerText = ""
+let board = [] //for one time process
 
 //clear
-const clearButton = document.querySelector('#clear-button')
-clearButton.addEventListener('click', function () {
-    display.innerText = ''
-    inputDigit = []
+const clear = function () {
+    display.innerText = ""
+    inputDigits = []
     board = []
-})
+}
+//clear button
+const clearButton = document.querySelector('#clear')
+clearButton.addEventListener('click', clear)
 
-//equal
-const equalButton = document.querySelector('#equal-button')
-equalButton.addEventListener('click', function () {
-    board.push(inputDigit.join(''))
-    inputDigit = []
-    // console.log(board)
-    // console.log(board[0] + inputOperator + board[1])
-    switch (inputOperator) {
-        case "add":
-            // console.log(add(parseInt(board[0]), parseInt(board[1])))
-            display.innerText = add(parseInt(board[0]), parseInt(board[1]))
-            break;
-        case "subtract":
-            // console.log(subtract(parseInt(board[0]), parseInt(board[1])))
-            display.innerText = subtract(parseInt(board[0]), parseInt(board[1]))
-            break;
-        case "multiply":
-            // console.log(multiply(parseInt(board[0]), parseInt(board[1])))
-            display.innerText = multiply(parseInt(board[0]), parseInt(board[1]))
-            break;
-        case "divide":
-            // console.log(divide(parseInt(board[0]), parseInt(board[1])))
-            display.innerText = divide(parseInt(board[0]), parseInt(board[1]))
-            break;
+//input: digit
+let inputDigits = [] //for each digit input
+const digits = document.querySelectorAll('.digit');
+digits.forEach(digit => digit.addEventListener('click', function () {
+    inputDigits.push(digit.value)
+    // console.log(digit.value);
+    console.log(inputDigits);
+    display.innerText = inputDigits.join('')
+}))
+
+//input: operator
+const operators = document.querySelectorAll('#operator-container button');
+operators.forEach(operator => operator.addEventListener('click', function () {
+    board.push(inputDigits.join(''))
+    if (board[0] === '') {
+        console.log('wrong input, resetting')
+        clear()
+    }
+    else if (board.length === 3) {
+        result = operate(parseInt(board[0]), board[1], parseInt(board[2]))
+        console.log(`${board[0]} ${board[1]} ${board[2]} = ${result}`)
+        display.innerText = result
+        board = [result]
+        board.push(operator.value)
+        inputDigits = []
+    } else {
+        board.push(operator.value)
+        inputDigits = []
+        console.log(operator.value);
+    }
+}))
+
+//equal (enter)
+const equal = document.querySelector('#equal')
+equal.addEventListener('click', function () {
+    board.push(inputDigits.join(''))
+    if (board.length < 2 || board[2] === '') {
+        console.log('wrong input, resetting')
+        clear()
+    } else {
+        let result = operate(parseInt(board[0]), board[1], parseInt(board[2]))
+        console.log(`${board[0]} ${board[1]} ${board[2]} = ${result}`)
+        display.innerText = result
+        board = []
+        inputDigits = []
     }
 })
